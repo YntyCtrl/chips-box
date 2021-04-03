014238b51b48ee08c8310b1091f00306962226f4
YntyCtrl <yntymak.199848@gmai.com>
2021-04-03 18:25:22 +0600

    Copy all

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import Layout from "./components/Layout/Layout";
+import PizzaBuilder from "./components/PizzaBuilder/PizzaBuilder";
 
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      
+      <Layout>
+        <PizzaBuilder />
+      </Layout>
     </div>
   );
-}
+};
 
 export default App;
