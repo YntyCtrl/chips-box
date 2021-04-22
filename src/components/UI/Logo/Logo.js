9a6c44fe4e8c27ffe9540853651749c4c5b4ee3b
YntyCtrl <yntymak.199848@gmai.com>
2021-04-23 00:08:16 +0600

    changed logo and rename

--- src/components/UI/Logo/Logo.js
+++ src/components/UI/Logo/Logo.js
@@ -1,11 +1,11 @@
 import classes from "./Logo.module.css";
-import logo from "../../../image/Logo.png";
+import logo from "../../../image/logo.png";
 
 const Logo = () => {
   return (
     <div className={classes.Logo}>
       <img src={logo} alt="Logo of the Pizza project" />
-      <div>Pizza</div>
+      <div>Chips Box</div>
     </div>
   );
 }
