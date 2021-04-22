95e476360755e691d9383f2929b977fa9bb7687d
YntyCtrl <yntymak.199848@gmai.com>
2021-04-23 00:12:47 +0600

    changed Bulder

--- src/components/PizzaBuilder/PizzaBuilder.js
+++ src/components/PizzaBuilder/PizzaBuilder.js
@@ -4,27 +4,53 @@ import PizzaControls from "./PizzaControls/PizzaControls";
 import classes from "./PizzaBuilder.module.css";
 import { useState } from "react";
 
+import Modal from "../UI/Modal/Modal";
+import OrderSummary from "./OrderSummary/OrderSummary";
+import Button from "../UI/Button/Button";
+
 const PizzaBuilder = () => {
+  const prices = {
+    tomato: 3.5,
+    salami: 4,
+    greenOlive: 0.3,
+    blackOlive: 0.3,
+    redPepper: 2,
+    yellowPepper: 1,
+  };
   const [ingredients, setIngredients] = useState({
-    tomato: 0,
-    salami: 0,
-    greenOlive: 0,
-    blackOlive: 0,
-    redPepper: 0,
-    yellowPepper: 0,
+    Bekon: 0,
+    Barbecue: 0,
+    Crab: 0,
+    Hickory: 0,
+    Classic: 0,
+    Onion: 0,
   });
+
+  const [ordering, setOrdering] = useState(false);
+  const [price, setPrice] = useState(150);
 
   function addIngredient(type) {
     const newIngredients = { ...ingredients };
     newIngredients[type]++;
+    setPrice(price + prices[type]);
     setIngredients(newIngredients);
   }
 
   function removeIngredient(type) {
-    const newIngredients = { ...ingredients };
-    
-    newIngredients[type]--;
-    setIngredients(newIngredients);
+    if (ingredients[type]) {
+      const newIngredients = { ...ingredients };
+      newIngredients[type]--;
+      setPrice(price - prices[type]);
+      setIngredients(newIngredients);
+    }
+  }
+
+  function startOrdering() {
+    setOrdering(true);
+  }
+
+  function stopOrdering() {
+    setOrdering(false);
   }
 
   return (
@@ -34,7 +60,15 @@ const PizzaBuilder = () => {
         ingredients={ingredients}
         addIngredient={addIngredient}
         removeIngredient={removeIngredient}
-        />
+        startOrdering={startOrdering}
+      />
+      <Modal show={ordering} cancel={stopOrdering}>
+        <OrderSummary ingredients={ingredients} price={price} />
+        <Button  green>
+          Checkout
+        </Button>
+        <Button onClick={stopOrdering}>Cancel</Button>
+      </Modal>
     </div>
   );
 }
