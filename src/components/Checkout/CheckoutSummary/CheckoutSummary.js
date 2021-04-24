47accb012fba8165e1d60950982713a7ad1bf24e
YntyCtrl <yntymak.199848@gmai.com>
2021-04-24 17:52:29 +0600

    changed checkout

--- src/components/Checkout/CheckoutSummary/CheckoutSummary.js
+++ src/components/Checkout/CheckoutSummary/CheckoutSummary.js
@@ -1,16 +1,19 @@
 import classes from "./CheckoutSummary.module.css";
-import PizzaPreview from "../../PizzaBuilder/PizzaPreview/PizzaPreview";
+import ChipsPreview from "../../CipsBox/ChipsPreview/ChipsPreview";
 import Button from "../../UI/Button/Button";
 
 const CheckoutSummary = ({ cancelCallback }) => {
   return (
     <div className={classes.CheckoutSummary}>
       <div>
-        <PizzaPreview ingredients={{
-          Bekon: 5, 
-          Barbecue: 10,
-          Classic: 50,
-        }} price={2477} />
+        <ChipsPreview
+          tastes={{
+            Bekon: 5,
+            Barbecue: 10,
+            Classic: 50,
+          }}
+          price={2477}
+        />
       </div>
       <div>
         <Button>Checkout</Button>
