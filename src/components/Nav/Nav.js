9e51ceefe662f21cc382b500e226ff72812d8a96
YntyCtrl <yntymak.199848@gmai.com>
2021-04-24 14:58:10 +0600

    changed Nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,15 @@
 import classes from "./Nav.module.css";
+import NavItem from "./NavItem/NavItem";
 
 const Nav = () => {
   return (
-    <div className={classes.Nav}>Nav</div>
+    <ul className={classes.Nav}>
+      <NavItem url="/" exact>
+        Builder
+      </NavItem>
+      <NavItem url="/checkout">Checkout</NavItem>
+    </ul>
   );
-}
+};
 
 export default Nav;
