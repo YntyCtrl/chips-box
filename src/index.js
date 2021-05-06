1779226bdcc656ff4787324695505aac34350f58
YntyCtrl <yntymak.199848@gmai.com>
2021-05-06 23:56:56 +0600

    import provider redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import chipsReducer from "./store/chipsReducer";
+
+
+const store = createStore(chipsReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
