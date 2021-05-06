85f0b3e90fe05cbf1089ad0dbb00c651dea5ee0a
YntyCtrl <yntymak.199848@gmai.com>
2021-05-07 00:06:36 +0600

    changed button

--- src/components/CipsBox/ChipsControls/ChipsControl/ChipsControl.js
+++ src/components/CipsBox/ChipsControls/ChipsControl/ChipsControl.js
@@ -1,15 +1,24 @@
 import Button from "../../../UI/Button/Button";
 import PizzaTaste from "../../ChipsTaste/ChipsTaste";
 import classes from "./ChipsControl.module.css";
+import { useDispatch } from "react-redux";
 
-const ChipsControl = ({ type, add, remove, count }) => {
+const ChipsControl = ({ type,  count }) => {
+
+  const dispatch = useDispatch();
+
   return (
     <div className={classes.ChipsControl}>
-      <Button onClick={() => add(type)}>+</Button>
+      <Button onClick={() => dispatch({ type: "ADD_INGREDIENT", taste: type })}>
+        +
+      </Button>
       <div className={classes.taste}>
         <PizzaTaste type={type} fixed />
       </div>
-      <Button onClick={() => remove(type)} disabled={!count}>
+      <Button
+        onClick={() => dispatch({ type: "REMOVE_INGREDIENT", taste: type })}
+        disabled={!count}
+      >
         -
       </Button>
     </div>
