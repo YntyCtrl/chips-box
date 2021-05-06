a5ff99a770cf395f4a0e928e9e76196ab83a1df7
YntyCtrl <yntymak.199848@gmai.com>
2021-05-07 00:05:48 +0600

    deleted add and remove testes

--- src/components/CipsBox/ChipsControls/ChipsControls.js
+++ src/components/CipsBox/ChipsControls/ChipsControls.js
@@ -4,21 +4,17 @@ import classes from "./ChipsControls.module.css";
 
 const ChipsControls = ({
   tastes,
-  addIngredient,
-  removeIngredient,
   startOrdering,
 }) => {
   const results = [];
   let total = 0;
   for (const taste in tastes) {
-    // Add ingredient number to totals number
+    
     total += tastes[taste];
-    // Render pizza control for this ingredient
+    
     results.push(
       <ChipsControl
         key={taste}
-        add={addIngredient}
-        remove={removeIngredient}
         count={tastes[taste]}
         type={taste}
       />
