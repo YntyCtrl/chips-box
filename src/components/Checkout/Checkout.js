5dd16fd8b5faceb6ef6ce4d6fff0242cb5de9d8f
YntyCtrl <yntymak.199848@gmai.com>
2021-05-07 00:03:57 +0600

    use ubmit and cancel

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,13 +1,40 @@
-import CheckoutSummary from "./CheckoutSummary/CheckoutSummary"
+import classes from "./Checkout.module.css";
+import ChipsPreview from "../CipsBox/ChipsPreview/ChipsPreview";
+import ChekoutForm from "./ChekoutForm/CheckoutForm";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const Checkout = ({ history }) => {
+  const tastes = useSelector((state) => state.tastes);
+  const price = useSelector((state) => state.price);
   function cancelCallback() {
     history.replace('/');
   }
+  function submitCallback(event) {
+    const data = new FormData(event.target);
+
+    axios
+      .post("https://chips-box-default-rtdb.firebaseio.com/orders.json", {
+        name: data.get("name"),
+        address: data.get("address"),
+        phone: data.get("phone"),
+        tastes: tastes,
+        price: price,
+      })
+      .then((response) => {
+        history.replace("/");
+      });
+
+    event.preventDefault();
+  }
 
   return (
-    <div>
-      <CheckoutSummary cancelCallback={cancelCallback} />
+    <div className={classes.Checkout}>
+      <ChipsPreview tastes={tastes} price={price} />
+      <ChekoutForm
+        cancelCallback={cancelCallback}
+        submitCallback={submitCallback}
+      />
     </div>
   );
 }
