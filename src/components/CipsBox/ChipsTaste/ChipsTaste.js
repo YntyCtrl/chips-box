f6445c3c2ffb6fdbf836b809f3be487fdcf9fc89
YntyCtrl <yntymak.199848@gmai.com>
2021-05-07 00:07:09 +0600

    deleted coment

--- src/components/CipsBox/ChipsTaste/ChipsTaste.js
+++ src/components/CipsBox/ChipsTaste/ChipsTaste.js
@@ -41,34 +41,7 @@ const ChipsTaste = ({ type, fixed }) => {
     },
   };
 
-  // function getPosition(ingredientWidth) {
-  //   // const pizzaDiameter = 380;
-  //   // const pizzaRadius = pizzaDiameter / 2;
-  //   // const ingredientRadius = parseInt(ingredientWidth) / 2;
-
-  //   // const ingredientTop = Math.round(Math.random() * pizzaDiameter);
-  //   // const ingredientLeft = Math.round(Math.random() * pizzaDiameter);
-
-  //   const distance = Math.sqrt(
-  //     Math.pow(ingredientTop - pizzaRadius, 2) + Math.pow(ingredientLeft - pizzaRadius, 2)
-  //   ) + ingredientRadius;
-
-  //   return distance < pizzaRadius
-  //     ? {
-  //       top: ingredientTop - ingredientRadius,
-  //       left: ingredientLeft - ingredientRadius
-  //     }
-  //     : getPosition(ingredientWidth);
-  // }
-
-  // Get random position for this ingredient.
-  // if (!fixed) {
-  //   const position = getPosition(types[type].width);
-  //   types[type].top = position.top + "px";
-  //   types[type].left = position.left + "px";
-  // }
-  // Get random rotation for this ingredient.
-  // types[type].transform = `rotate(${Math.round(Math.random() * 360)}deg)`;
+ 
 
   return <div className={classes.ChipsTaste} style={types[type]}></div>;
 };
