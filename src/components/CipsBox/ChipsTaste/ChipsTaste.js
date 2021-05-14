298dcdc6f31b8787b1f606ec540d3d76eb56f4b1
YntyCtrl <yntymak.199848@gmai.com>
2021-05-14 14:15:04 +0600

    swap barbecue for fanta

--- src/components/CipsBox/ChipsTaste/ChipsTaste.js
+++ src/components/CipsBox/ChipsTaste/ChipsTaste.js
@@ -2,48 +2,59 @@ import React from "react";
 import classes from "./ChipsTaste.module.css";
 
 import BekonBackground from "../../../image/Bekon.png";
-import Barbecue from "../../../image/Barbecue.png";
+import Fanta from "../../../image/fanta.png";
 import СrabBackground from "../../../image/Crab.png";
 import Hickory from "../../../image/Hickory.png";
 import Classic from "../../../image/Classic.png";
 import Onion from "../../../image/Onion.png";
-const ChipsTaste = ({ type, fixed }) => {
+const ChipsTaste = ({ type }) => {
   const types = {
     Bekon: {
       backgroundImage: `url(${BekonBackground})`,
       width: "50px",
       height: "60px",
+      borderBottom: "5px solid rgb(128, 121, 24)",
     },
     Barbecue: {
-      backgroundImage: `url(${Barbecue})`,
-      width: "50px",
+      backgroundImage: `url(${Fanta})`,
+      
+      width: "17px",
       height: "60px",
+      borderBottom: "5px solid rgb(128, 121, 24)",
     },
     Crab: {
       backgroundImage: `url(${СrabBackground})`,
       width: "50px",
       height: "60px",
+      borderBottom: "5px solid rgb(128, 121, 24)",
     },
     Hickory: {
       backgroundImage: `url(${Hickory})`,
       width: "50px",
       height: "60px",
+      borderBottom: "5px solid rgb(128, 121, 24)",
     },
     Classic: {
       backgroundImage: `url(${Classic})`,
       width: "50px",
       height: "60px",
+      borderBottom: "5px solid rgb(128, 121, 24)",
     },
     Onion: {
       backgroundImage: `url(${Onion})`,
       width: "50px",
       height: "60px",
+      borderBottom: "5px solid rgb(128, 121, 24)",
     },
   };
 
  
 
-  return <div className={classes.ChipsTaste} style={types[type]}></div>;
+  return (
+    <div className={classes.ChipsTaste} style={types[type]}>
+      <div className={classes.holder}></div>
+    </div>
+  );
 };
 
 export default React.memo(ChipsTaste);
