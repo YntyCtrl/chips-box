ef7d0d438efb8a5b16d24b2f9a2a15e056a5f79b
YntyCtrl <yntymak.199848@gmai.com>
2021-05-25 16:24:49 +0600

    changed navigation

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,17 @@
 import classes from "./Nav.module.css";
 import NavItem from "./NavItem/NavItem";
+import { useSelector } from "react-redux";
 
 const Nav = () => {
+  const isAuthenticated = useSelector((state) => state.auth.token !== null);
   return (
     <ul className={classes.Nav}>
       <NavItem url="/" exact>
         Box made
       </NavItem>
-      <NavItem url="/orders">Orders</NavItem>
+      {!isAuthenticated ? <NavItem url="/auth">Login</NavItem> : null}
+      {isAuthenticated ? <NavItem url="/orders">Orders</NavItem> : null}
+      {isAuthenticated ? <NavItem url="/logout">Logout</NavItem> : null}
     </ul>
   );
 };
