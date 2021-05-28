e86d78c3b9b4bfbd2ae195b698f296612cbe4393
YntyCtrl <yntymak.199848@gmai.com>
2021-05-28 14:13:04 +0600

    fixed error

--- src/components/Orders/Order/Order.js
+++ src/components/Orders/Order/Order.js
@@ -2,13 +2,13 @@ import classes from "./Order.module.css";
 
 const Order = ({ name, phone, address, tastes, price }) => {
     const outputIngredients = Object.keys(tastes)
-        .map(taste => <em>{taste} - {tastes[taste]}</em>);
+        .map(taste => <em key={taste}>({taste} - {tastes[taste]}), </em>);
 
     return (
         <div className={classes.Order}>
-            <div>{name}, {phone}, {address}</div>
+            <div className={classes.name}>{name}, {phone}, {address}</div>
             <div>{outputIngredients}</div>
-            <strong>{price}</strong>
+            <strong>{price}som</strong>
         </div>
     );
 }
